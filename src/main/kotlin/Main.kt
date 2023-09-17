29dacd79fd6e7f0911ada24aef5cf7509fb2cf24
boring-km <kms0644804@naver.com>
2023-09-17 22:16:46 +0900

    refactor: apply Extract method (chapter 3.2)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
@@ -171,43 +172,54 @@ fun update(
 @Composable
 fun draw(mapState: MutableState<Array<Array<Int>>>, playerxState: MutableState<Int>, playeryState: MutableState<Int>) {
     Canvas(modifier = Modifier.width(1200.dp).height(800.dp)) {
-        val map = mapState.value
-        val playerx = playerxState.value
-        val playery = playeryState.value
+        drawMap(mapState)
+        drawPlayer(playerxState, playeryState)
+    }
+}
 
-        for (y in map.indices) {
-            for (x in map[y].indices) {
-                val rect = Rect(
-                    (x * TITLE_SIZE).toFloat(),
-                    (y * TITLE_SIZE).toFloat(),
-                    TITLE_SIZE.toFloat(),
-                    TITLE_SIZE.toFloat()
-                )
-                val color = if (map[y][x] == Tile.FLUX.ordinal) {
-                    Color(0xffccffcc)
-                } else if (map[y][x] == Tile.UNBREAKABLE.ordinal) {
-                    Color(0xff999999)
-                } else if (map[y][x] == Tile.STONE.ordinal || map[y][x] == Tile.FALLING_STONE.ordinal) {
-                    Color(0xff0000cc)
-                } else if (map[y][x] == Tile.BOX.ordinal || map[y][x] == Tile.FALLING_BOX.ordinal) {
-                    Color(0xff8b4513)
-                } else if (map[y][x] == Tile.KEY1.ordinal || map[y][x] == Tile.LOCK1.ordinal) {
-                    Color(0xffffcc00)
-                } else if (map[y][x] == Tile.KEY2.ordinal || map[y][x] == Tile.LOCK2.ordinal) {
-                    Color(0xff00ccff)
-                } else {
-                    Color.White
-                }
-                drawRect(color = color, topLeft = rect.topLeft, size = Size(TITLE_SIZE.toFloat(), TITLE_SIZE.toFloat()))
+private fun DrawScope.drawPlayer(
+    playerxState: MutableState<Int>,
+    playeryState: MutableState<Int>
+) {
+    val playerx = playerxState.value
+    val playery = playeryState.value
+    val player = Rect(
+        playerx * TITLE_SIZE.toFloat(),
+        playery * TITLE_SIZE.toFloat(),
+        TITLE_SIZE.toFloat(),
+        TITLE_SIZE.toFloat()
+    )
+    drawRect(color = Color(0xffff0000), player.topLeft, Size(TITLE_SIZE.toFloat(), TITLE_SIZE.toFloat()))
+}
+
+private fun DrawScope.drawMap(mapState: MutableState<Array<Array<Int>>>) {
+    val map = mapState.value
+
+    for (y in map.indices) {
+        for (x in map[y].indices) {
+            val rect = Rect(
+                (x * TITLE_SIZE).toFloat(),
+                (y * TITLE_SIZE).toFloat(),
+                TITLE_SIZE.toFloat(),
+                TITLE_SIZE.toFloat()
+            )
+            val color = if (map[y][x] == Tile.FLUX.ordinal) {
+                Color(0xffccffcc)
+            } else if (map[y][x] == Tile.UNBREAKABLE.ordinal) {
+                Color(0xff999999)
+            } else if (map[y][x] == Tile.STONE.ordinal || map[y][x] == Tile.FALLING_STONE.ordinal) {
+                Color(0xff0000cc)
+            } else if (map[y][x] == Tile.BOX.ordinal || map[y][x] == Tile.FALLING_BOX.ordinal) {
+                Color(0xff8b4513)
+            } else if (map[y][x] == Tile.KEY1.ordinal || map[y][x] == Tile.LOCK1.ordinal) {
+                Color(0xffffcc00)
+            } else if (map[y][x] == Tile.KEY2.ordinal || map[y][x] == Tile.LOCK2.ordinal) {
+                Color(0xff00ccff)
+            } else {
+                Color.White
             }
+            drawRect(color = color, topLeft = rect.topLeft, size = Size(TITLE_SIZE.toFloat(), TITLE_SIZE.toFloat()))
         }
-        val player = Rect(
-            playerx * TITLE_SIZE.toFloat(),
-            playery * TITLE_SIZE.toFloat(),
-            TITLE_SIZE.toFloat(),
-            TITLE_SIZE.toFloat()
-        )
-        drawRect(color = Color(0xffff0000), player.topLeft, Size(TITLE_SIZE.toFloat(), TITLE_SIZE.toFloat()))
     }
 }
 
